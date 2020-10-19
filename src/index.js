c9baaf27338cade19c26803431b39a6cdd573f75
Eugeniy Bekanurovich <passivevoice3@gmail.com>
2020-10-19 20:55:05 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let result = '';
+
+    for(let i = 0; i < expr.length + 1; i += 10) { 
+        if(expr.substring(i, i + 10) !== '') {
+            arr.push(expr.substring(i, i + 10))
+        }
+    }  
+
+    arr.map(item => {
+    let temp = '';
+    for(let i = 0; i < item.length - 1; i += 2) {
+        if(item[i] + item[i + 1] === '10') {
+            temp += '.';
+        } else if (item[i] + item[i + 1] === '11') {
+            temp += '-';
+        } else if(item[i] === '*') {
+            temp += ' ';
+            break;
+        }
+    }
+
+        temp === ' ' ? result += ' ' : result += MORSE_TABLE[temp];
+    })
+
+    return result;
 }
 
 module.exports = {
